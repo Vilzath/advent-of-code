be8acbaae838b06656af06f3729a164a84468477
SFML_Azynd <maximilien.sauvage@outlook.com>
2023-12-08 11:03:48 +0100

    Day 2-1

---
+++ 2023/Day 2/1/index.js
@@ -0,0 +1,87 @@
+const fs = require('fs');
+
+var realFile = fs.readFileSync("input.txt", { encoding: 'utf8' })
+var calibration = fs.readFileSync("calibration.txt", { encoding: 'utf8' })
+
+const RED = 12
+const GREEN = 13
+const BLUE = 14
+
+let result = 0
+
+let splt = realFile.split("\n")
+
+splt.forEach(str => {
+
+    const idAndNot = str.split(":")
+    let isValidHand = true;
+
+    let gameId = Number(idAndNot[0].replaceAll(":", "").replaceAll("Game ", ""));
+    let completeTirage = idAndNot[1].replaceAll(" ", "").replaceAll(",", " ");
+    let hands = completeTirage.split(";")
+
+    hands.forEach(hand => {
+        if (!isValidHand) return;
+
+        const indexB = hand.indexOf("blue")
+        const indexG = hand.indexOf("green")
+        const indexR = hand.indexOf("red")
+
+        let countR = 0
+        let countG = 0
+        let countB = 0
+
+        if (indexB != -1) {
+            let tmp = ""
+            let i = 1
+            while (hand.charAt(indexB - i) != " ") {
+                if (indexB - i > -1) {
+                    tmp = hand.charAt(indexB - i) + tmp
+
+                } else {
+                    break
+                }
+                i = i + 1
+            }
+            countB = Number(tmp)
+        }
+        if (indexR != -1) {
+            let tmp = ""
+            let i = 1
+            while (hand.charAt(indexR - i) != " ") {
+                if (indexR - i > -1) {
+                    tmp = hand.charAt(indexR - i) + tmp
+
+                } else {
+                    break
+                }
+                i = i + 1
+            }
+            countR = Number(tmp)
+        }
+        if (indexG != -1) {
+            let tmp = ""
+            let i = 1
+            while (hand.charAt(indexG - i) != " ") {
+                if (indexG - i > -1) {
+                    tmp = hand.charAt(indexG - i) + tmp
+
+                } else {
+                    break
+                }
+                i = i + 1
+            }
+            countG = Number(tmp)
+        }
+        console.log("r : " + countR + " g : " + countG + " b :" + countB)
+        if (countR > RED || countG > GREEN || countB > BLUE) {
+            isValidHand = false;
+        }
+    })
+
+    if (isValidHand) {
+        result = result + gameId
+    }
+})
+
+console.log(result)
